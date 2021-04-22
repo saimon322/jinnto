01f7b6f069890df0e117310ba7d2bdfc8d436d1a
Tyurin <tyurin@profit-lab.ru>
2021-04-22 22:32:12 +0300

    toggler

--- src/js/switch-panel.js
+++ src/js/switch-panel.js
@@ -1,15 +1,18 @@
 const switchPanelSelector = '.banner__switch-panel';
 const switchPanelClass = 'banner__switch-panel--switched';
 const switchPanels = document.querySelectorAll(switchPanelSelector);
+const contentBox = document.querySelector('.gift-content');
+const nextContentClass = 'gift-content--sub-content';
+const animationClass = 'gift-content-fade';
 
 if (switchPanels) {
     switchPanels.forEach(item => {
-        item.addEventListener('click', () => {
-            if (!item.classList.contains(switchPanelClass)) {
-                item.classList.add(switchPanelClass);
-            } else {
-                item.classList.remove(switchPanelClass)
-            }
-        })
+        const switcher = () => {
+            item.classList.toggle(switchPanelClass);
+            contentBox.classList.toggle(nextContentClass);
+        };
+
+        item.addEventListener('click', switcher)
     });
+
 }
